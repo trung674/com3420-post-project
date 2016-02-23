a8df75641b9782a6bb93ef409e7ac0365c0561ea
Andrew Cook <asacook1@sheffield.ac.uk>
2016-02-23 11:57:36 +0000

    Errbit configured

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,6 @@
+Airbrake.configure do |config|
+ config.api_key = ab841fcc62027bace6eca9606f4714b8 
+ config.host = 'errbit.software-hut.org.uk'
+ config.post = 443
+ config.secure = config.post == 443
+end
