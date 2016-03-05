baf1195d57b3cd48daa82a6dabc99bd20ae6ca0d
Harry Howarth <hhowarth1@sheffield.ac.uk>
2016-03-05 12:13:18 +0000

    Changed the application haml to no longer say lab4 etc...

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Lab4
+module Project
   class Application < Rails::Application
     # Send queued jobs to delayed_job
     config.active_job.queue_adapter = :delayed_job
