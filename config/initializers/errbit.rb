586590f660733660a57669bdb4e6a34be6c674c8
Harry Howarth <hhowarth1@sheffield.ac.uk>
2016-03-06 20:18:12 +0000

    errbit update, dont understand

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,18 @@
 Airbrake.configure do |config|
+
+  # our hacky version - gitignore is not working for me :(
   config.project_id = 'project'
   config.project_key = 'ab841fcc62027bace6eca9606f4714b8'
   config.host = 'errbit.software-hut.org.uk'
   # config.post = 443
   # config.secure = config.post == 443
+
+
+  # proper version
+  # config.api_key = ab841fcc62027bace6eca9606f4714b8
+  # config.host = 'errbit.software-hut.org.uk'
+  # config.post = 443
+  # config.secure = config.post == 443
+
+
 end
