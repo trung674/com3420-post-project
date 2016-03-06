02948a2eca3d717cfb3d821aafe49538ca72f377
Danny Heard <dheard2@sheffield.ac.uk>
2016-03-06 16:36:09 +0000

    Git didn't ignore it

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,6 @@
 Airbrake.configure do |config|
- config.project_id = 'project'
- config.project_key = 'ab841fcc62027bace6eca9606f4714b8'
- config.host = 'errbit.software-hut.org.uk'
- # config.post= 443
- # config.secure = config.post == 443
+  config.api_key = ab841fcc62027bace6eca9606f4714b8
+  config.host = 'errbit.software-hut.org.uk'
+  config.post = 443
+  config.secure = config.post == 443
 end
