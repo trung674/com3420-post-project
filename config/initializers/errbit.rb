19b538d389bdc2bdff28b89a91ade19f527ffba2
Harry Howarth <hhowarth1@sheffield.ac.uk>
2016-03-06 13:46:47 +0000

    map center changed again

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,7 @@
 Airbrake.configure do |config|
- config.api_key = ab841fcc62027bace6eca9606f4714b8 
- config.host = 'errbit.software-hut.org.uk'
- config.post = 443
- config.secure = config.post == 443
+  config.project_id = 'project'
+  config.project_key = 'ab841fcc62027bace6eca9606f4714b8'
+  config.host = 'errbit.software-hut.org.uk'
+  # config.post = 443
+  # config.secure = config.post == 443
  end
