cf00941eb3f528c2dd9b40604cd1e978298d4a97
Trung Nguyen Thanh <tnguyenthanh1@sheffield.ac.uk>
2016-03-07 12:17:00 +0000

    Add header, footer, change application.css to application.sass

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,13 +1,4 @@
 Airbrake.configure do |config|
-<<<<<<< HEAD
- config.project_id = 'project'
- config.project_key = '12345'
- #config.host = 'errbit.software-hut.org.uk'
- #config.post = 443
- #config.secure = config.post == 443
- end
-=======
-
   # our hacky version - gitignore is not working for me :(
   config.project_id = 'project'
   config.project_key = 'ab841fcc62027bace6eca9606f4714b8'
@@ -24,4 +15,3 @@ Airbrake.configure do |config|
 
 
 end
->>>>>>> d7f0e39fe87fe2ef6214cabc7a8ecec12680d88c
