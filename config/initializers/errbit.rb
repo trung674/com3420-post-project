088947fb7ef8f444cadf8f9ef37ec22f078a6803
Danny <dheard2@sheffield.ac.uk>
2016-03-08 20:03:42 +0000

    Trying to deploy

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,17 +1,17 @@
 Airbrake.configure do |config|
   # our hacky version - gitignore is not working for me :(
-  config.project_id = 'project'
-  config.project_key = 'ab841fcc62027bace6eca9606f4714b8'
-  config.host = 'errbit.software-hut.org.uk'
+  # config.project_id = 'project'
+  # config.project_key = 'ab841fcc62027bace6eca9606f4714b8'
+  # config.host = 'errbit.software-hut.org.uk'
   # config.post = 443
   # config.secure = config.post == 443
 
 
   # proper version
-  # config.api_key = ab841fcc62027bace6eca9606f4714b8
-  # config.host = 'errbit.software-hut.org.uk'
-  # config.post = 443
-  # config.secure = config.post == 443
+  config.api_key = ab841fcc62027bace6eca9606f4714b8
+  config.host = 'errbit.software-hut.org.uk'
+  config.post = 443
+  config.secure = config.post == 443
 
 
 end
