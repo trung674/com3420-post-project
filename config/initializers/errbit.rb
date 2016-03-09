d7582959b06ad1307308840b26bc09e00551dde2
Trung Nguyen Thanh <tnguyenthanh1@sheffield.ac.uk>
2016-03-09 10:08:24 +0000

    small errbit change

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -8,10 +8,8 @@ Airbrake.configure do |config|
 
 
   # proper version
-  config.api_key = 'ab841fcc62027bace6eca9606f4714b8'
+  config.api_key = ab841fcc62027bace6eca9606f4714b8
   config.host = 'errbit.software-hut.org.uk'
   config.post = 443
   config.secure = config.post == 443
-
-
 end
