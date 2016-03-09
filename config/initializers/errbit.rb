28765fcf49599fad67ad3d69294a0fffd146243f
Danny Heard <dheard2@sheffield.ac.uk>
2016-03-09 11:44:14 +0000

    Final errbit change

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -10,6 +10,6 @@ Airbrake.configure do |config|
   # proper version
   config.api_key = 'ab841fcc62027bace6eca9606f4714b8'
   config.host = 'errbit.software-hut.org.uk'
-  config.post = 443
-  config.secure = config.post == 443
+  config.port = 443
+  config.secure = config.port == 443
 end
