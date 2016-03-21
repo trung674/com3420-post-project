8b600a18ab775e4acdc06ca917dc5d1b76e7592f
aca14asc <asacook1@sheffield.ac.uk>
2016-03-21 17:47:08 +0000

    Mods table added using devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321165031) do
+ActiveRecord::Schema.define(version: 20160321174313) do
 
   create_table "contributors", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +53,24 @@ ActiveRecord::Schema.define(version: 20160321165031) do
   end
 
   add_index "media", ["contributor_id"], name: "index_media_on_contributor_id"
+
+  create_table "mods", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "mods", ["email"], name: "index_mods_on_email", unique: true
+  add_index "mods", ["reset_password_token"], name: "index_mods_on_reset_password_token", unique: true
 
   create_table "records", force: :cascade do |t|
     t.string   "title"
