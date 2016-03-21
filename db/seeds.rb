44851beac16c7734b5fb91d1c4230c2e7c3e1a87
aca14asc <asacook1@sheffield.ac.uk>
2016-03-21 18:04:46 +0000

    made myself a moderator for now, for development purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Mod.where(email:
+ 'asacook1@sheffield.ac.uk').first_or_create(password:
+ 'password123', password_confirmation: 'password123')
