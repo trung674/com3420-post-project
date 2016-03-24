6114c903dfbf2ae582e70174f701fb15b8685d9a
Harry Howarth <hhowarth1@sheffield.ac.uk>
2016-03-24 22:08:19 +0000

    Search functionality works well now. Could be more scope for making location intelligent and adding dates to the search.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,19 +4,27 @@ class SearchController < ApplicationController
   def search
     # TODO search logic will go here
     @search = [params[:search]]
-    @type = [params[:type]]
+    type = [params[:type]]
     # More efficient to search by type first
-    medium_results = (Medium.where :type => @type)
+    medium_results = (Medium.where :type => type)
     ids = medium_results.ids
     records = Record.where('(location LIKE ? OR description LIKE ? OR title LIKE ?) AND medium_id IN (?)',
                             "%#{@search[0]}%", "%#{@search[0]}%", "%#{@search[0]}%", ids)
-  #   TODO decide what is in the return hash - see below
+    medium_ids = []
+    records.each do |getId|
+      medium_ids.append(getId.medium_id)
+    end
 
-  #   linked filename - Title
-  #   date
-  #   location (add link)
-  #   Type
+    medium_final = Medium.where('id IN (?)', medium_ids)
 
-  #   TODO have a return hash
+    # This is the array of hashes that we send to the view based on the search.
+    @results_hashes = []
+    
+    # for loops create the array of the useful infomation. More efficient than passing objects.
+    for x in 0..(records.length-1)
+      @results_hashes.append({:title => records[x].title, :url => medium_final[x].upload,
+                             :date => records[x].ref_date, :location => records[x].location, :type =>medium_final[x].type })
+    end
+
   end
 end
