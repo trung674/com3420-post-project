db82cfa1257ef746488c7ae942d438cda967e82a
Harry Howarth <hhowarth1@sheffield.ac.uk>
2016-03-24 21:37:00 +0000

    Updated the search function. Now works with types.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,19 @@ class SearchController < ApplicationController
   def search
     # TODO search logic will go here
     @search = [params[:search]]
-    @records = Record.where('location LIKE ? OR description LIKE ? OR title LIKE ?', "%#{@search[0]}%", "%#{@search[0]}%", "%#{@search[0]}%")
+    @type = [params[:type]]
+    # More efficient to search by type first
+    medium_results = (Medium.where :type => @type)
+    ids = medium_results.ids
+    records = Record.where('(location LIKE ? OR description LIKE ? OR title LIKE ?) AND medium_id IN (?)',
+                            "%#{@search[0]}%", "%#{@search[0]}%", "%#{@search[0]}%", ids)
+  #   TODO decide what is in the return hash - see below
+
+  #   linked filename - Title
+  #   date
+  #   location (add link)
+  #   Type
+
+  #   TODO have a return hash
   end
 end
