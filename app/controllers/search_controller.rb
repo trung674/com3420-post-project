de3009c58dfbc51b166a64e55eca6b44efc2ec22
Harry Howarth <hhowarth1@sheffield.ac.uk>
2016-03-24 18:27:45 +0000

    added todo

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
 
 
   def search
+    # TODO search logic will go here
     @search = [params[:search]]
     @records = Record.where('location LIKE ? OR description LIKE ? OR title LIKE ?', "%#{@search[0]}%", "%#{@search[0]}%", "%#{@search[0]}%")
   end
