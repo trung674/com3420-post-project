8334e412c0e8116ad113cb2fc7c5a5b49a54ea72
Danny <dheard2@sheffield.ac.uk>
2016-03-29 16:46:51 +0100

    Created downloader job

--- app/uploaders/recording_uploader.rb
+++ app/uploaders/recording_uploader.rb
@@ -4,6 +4,7 @@ class RecordingUploader < MediumUploader
   after :store, :process_audio
 
   def process_audio(new_file)
-    convert_audio
+    # Queue the transcription
+    TranscriberUploaderJob.delay.perform(new_file, self.model)
   end
 end
