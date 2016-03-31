aed27cedbbe60fca55cbf7561519deb56dc15c4e
Danny <dheard2@sheffield.ac.uk>
2016-03-31 10:31:36 +0100

    Showing document media

--- app/uploaders/medium_uploader.rb
+++ app/uploaders/medium_uploader.rb
@@ -2,19 +2,25 @@ class MediumUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :file
+  after :store, :delete_tmp_dir
   after :remove, :delete_empty_dirs
 
   # Override the directory where uploaded files will be stored.
   def store_dir
-    "#{Rails.root}/uploads/#{model.class.to_s.underscore}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{model.id}"
+  end
+
+  # Remove tmp upload folders
+  def delete_tmp_dir(new_file)
+    # make sure we don't delete other things accidentally by checking the name pattern
+    if @cache_id_was.present? && @cache_id_was =~ /\A[\d]{8}\-[\d]{4}\-[\d]+\-[\d]{4}\z/
+      FileUtils.rm_rf(File.join(root, cache_dir, @cache_id_was))
+    end
   end
 
   # Cleanup, remove empty folders
   def delete_empty_dirs
     path = ::File.expand_path(store_dir, root)
-    Dir.delete(path)
-
-    path = ::File.expand_path(base_store_dir, root)
     Dir.delete(path)
   rescue
     true
