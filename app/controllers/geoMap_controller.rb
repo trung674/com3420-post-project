4413b23b6bf2bac91b957c3e833f79ed9127a24f
Harry Howarth <hhowarth1@sheffield.ac.uk>
2016-04-07 15:40:01 +0100

    updated the map and controller, there is now a table when you click on an item. Need to find best way to populate this

--- app/controllers/geoMap_controller.rb
+++ app/controllers/geoMap_controller.rb
@@ -3,11 +3,12 @@ class GeomapController < ApplicationController
   def map
     @current_nav_identifier = :map
     @lat_lng = []
+    @obj_array = []
     Record.all.each do |thing|
-      #need to check that its not nil, they might not have clicked on the map.
-      #this could be in controller but then it would be loaded on every static page.
+      #this check for the 'people' who didnt manage to click on the map when uploading
       if thing.latitude
         @lat_lng.append({lat: thing.latitude , lng: thing.longitude})
+        @obj_array.append(thing)
       end
     end
   end
