b954c023e1f4f879ede92cef4bcbaa22015fe3f2
Danny <dheard2@sheffield.ac.uk>
2016-04-07 13:15:26 +0100

    Commenting

--- app/jobs/transcriber_downloader_job.rb
+++ app/jobs/transcriber_downloader_job.rb
@@ -2,44 +2,51 @@ require 'rest-client'
 require 'tempfile'
 require 'zip'
 
+# webASR REST API: http://www.webasr.org/apimanual
 class TranscriberDownloaderJob < Struct.new(:src, :ses, :model)
 
   def perform
+    # Get the status from webASR using the src and ses values
     response = RestClient.post('http://www.webasr.org/getstatus',:email => ASR_EMAIL, :password => ASR_PASSWORD,
                                :src => src, :ses => ses)
 
+    # Returns a string so get the status section
     transcribe_status = response.to_str.partition('is: ').last.strip.downcase
 
     if transcribe_status.include? 'processing'
       raise 'Still processing'
     elsif transcribe_status.include? 'completed'
-      # Download the .zip file
+      # Transcription is complete so download the .zip file
       response = RestClient.post('http://www.webasr.org/getfile',:email => ASR_EMAIL, :password => ASR_PASSWORD,
                                  :src => src, :ses => ses)
 
+      # Store the zip file temporarily
       zip_file = Tempfile.new('downloaded')
       zip_file.binmode
       zip_file.write(response.to_s)
 
-      dir = File.dirname(model.upload.path)
+      upload_dir = File.dirname(model.upload.path)
 
+      # Extract the transcription files from the zip and store them in the uploads folder
       Zip::File.open(zip_file.path) do |file|
         file.each do |content|
           data = file.read(content)
           ext_name = File.extname(content.to_s)
 
+          # Have to save ascii and binary files differently
           if ext_name == '.pdf' or ext_name == '.ttml'
-            File.open(File.join(dir, 'transcript') + ext_name, 'wb') {|f| f.write(data) }
+            File.open(File.join(upload_dir, 'transcript') + ext_name, 'wb') {|f| f.write(data) }
           elsif ext_name == '.xml'
-            File.open(File.join(dir, 'transcript.xml'), 'w') {|f| f.write(data) }
+            File.open(File.join(upload_dir, 'transcript.xml'), 'w') {|f| f.write(data) }
           else
             puts content.to_s
           end
+
         end
       end
 
-      # Upload the transcript file using carrier wave, currently will be the .pdf file
-      model.transcript = File.join(dir, 'transcript.pdf')
+      # Upload the transcript file using carrier wave, currently will be the .pdf file. Then save the model
+      model.transcript = File.join(upload_dir, 'transcript.pdf')
 
       raise model.errors unless model.save!
     else
