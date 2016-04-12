7cab8a58f4f1b905b03b3131f5bc7615acdf62a9
Danny <dheard2@sheffield.ac.uk>
2016-04-12 12:27:05 +0100

    Editing of records now submits

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -29,6 +29,8 @@ class MediaController < ApplicationController
   def new
     @medium = Medium.new
     @medium.records.build
+
+    # This dosen't seem to work
     @medium.build_contributor(params[:contributor_attributes])
 
     # By default a new contribution is a recording
@@ -46,14 +48,15 @@ class MediaController < ApplicationController
           @accepted_mimes = '.pdf'
       when 'Image'
           @accepted_mimes = '.jpeg,.jpg,.gif,.tff,.bmp,.png'
+      else
+        @accepted_mimes = ''
     end
-
   end
 
   def create
-    # Could be *slightly* hacky, will need thorough testing!
+    # When creating, the form could submit with or without a type so handle both
     if params[:medium].present?
-      @medium = Medium.new(medium_params("Medium"))
+      @medium = Medium.new(medium_params('Medium'))
     elsif params[:type].present?
       @medium = Medium.new(medium_params(params[:type]))
     end
@@ -71,30 +74,49 @@ class MediaController < ApplicationController
     if verify_recaptcha(model: @medium) && @medium.save
       redirect_to root_url, notice: 'Upload successful, please wait for approval'
     else
+      # TODO: Error messages
       render :new
     end
   end
 
+  # TODO: Some way for admins to see the unapproved records
   def show
     @medium = Medium.where(id: params[:id]).first
 
     @approved_records = @medium.records.where(approved: true).order('created_at DESC')
 
-    # TODO: change current record depending on selected
+    # Change current record depending on selected
     if params.has_key?(:record_id)
       @current_record = @approved_records.find(params[:record_id])
     else
       @current_record = @approved_records.first
     end
-
   end
 
   def edit
-    @medium = Medium.find(params[:id])
+    # When editing the most recent record for the medium is displayed
+    @medium = Medium.where(id: params[:id]).first
     @current_record = @medium.records.where(approved: true).order('created_at').last
   end
 
   def update
+    @medium = Medium.where(id: params[:id]).first
+
+    if params[:medium].present?
+      @record = Record.new(record_params(params['medium'])['record'])
+    elsif params[:type].present?
+      @record = Record.new(record_params(params[:type])['record'])
+    end
+
+    @record.medium = @medium
+
+    if verify_recaptcha(model: @medium) && @record.save
+      redirect_to root_url, notice: 'Edit successful, please wait for approval'
+    else
+      # TODO: Error messages
+      @current_record = @record
+      render :edit
+    end
   end
 
   private
@@ -105,5 +127,10 @@ class MediaController < ApplicationController
                                                     :text, records_attributes: [:title, :location, :description,
                                                                                 :latitude, :longitude, :ref_date],
                                                     contributor_attributes: [:name, :email, :phone])
+    end
+
+    def record_params(type)
+      # Permit parameters for editing records
+      params.require(type.underscore.to_sym).permit(record: [:title, :location, :description, :latitude, :longitude, :ref_date])
     end
 end
