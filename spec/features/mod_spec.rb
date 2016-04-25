b4695a2c4531e34598b1f75136acef329a81ff4e
aca14asc <asacook1@sheffield.ac.uk>
2016-04-25 17:06:50 +0100

    Specified some capybara tests

---
+++ spec/features/mod_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'Mod' do
+  
+  specify 'I can log in' do
+  end
+  
+  specify 'I can view moderators' do
+  end
+
+  specify 'I can activate moderator account' do
+  end
+
+  specify 'I can deactivate moderator account' do
+  end
+
+  specify 'I can approve record' do
+  end
+
+  specify 'I can reject record' do
+  end
+
+  specify 'I can upload new wallpaper' do
+  end
+
+  specify 'I can log out' do
+  end
+
+  specify 'I can create an inactive account' do
+  end
+
+  specify 'I cannot login with an inactive account' do
+  end
+
+end
--- spec/models/mod_spec.rb
+++ spec/models/mod_spec.rb
@@ -27,5 +27,6 @@
 require 'rails_helper'
 
 RSpec.describe Mod, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
 end
