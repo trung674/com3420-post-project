37879e7fcf7980cc1af6630b571959537e51eb17
Trung Nguyen Thanh <tnguyenthanh1@sheffield.ac.uk>
2016-04-25 22:35:30 +0100

    real seed data D:

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Data for editable content
 EditableContent.create([{
   name: "about",
   content: "<h2>Test About Page</h2>
@@ -17,7 +19,18 @@ EditableContent.create([{
   <li>Us</li>
 </ul>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-"
+"},
+{
+  name: "contact_address",
+  content: "23 Edward Street, Sheffield, UK, S3 7SF"
+},
+{
+  name: "contact_phone",
+  content: "(+44) 08827364723"
+},
+{
+  name: "working_hour",
+  content: "Monday - Friday: 9:00 AM to 5:00 PM"
 }])
 
 Mod.where(email:
