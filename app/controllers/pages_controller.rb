c76792c8f67dd3ece99b7b7149474c56f70e3d09
Harry Howarth <hhowarth1@sheffield.ac.uk>
2016-04-27 12:59:10 +0100

    search works on localhost, i think if you remove 'public' from the file url it will work on the server even if the file isnt there

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -172,12 +172,30 @@ class PagesController < ApplicationController
     # Function that takes recording's medium ids and searches the transcripts for matches
     if ids
       trans_search_hits = []
-      ids.each do |med_id|
-        # looks through each directory to find a file with an xml ending.
-        (Dir.entries('uploads/recording/'+''+med_id.to_s)).each do |name|
-          if name=~/.*\.xml$/
-            # read in the xml - label tags only! <label></label>
-            labels = Nokogiri::XML(File.open('uploads/recording/'+''+med_id.to_s+'/'+name)).xpath('//label')
+      # ids.each do |med_id|
+      #   # looks through each directory to find a file with an xml ending.
+      #   (Dir.entries('uploads/recording/'+''+med_id.to_s)).each do |name|
+      #     if name=~/.*\.xml$/
+      #       # read in the xml - label tags only! <label></label>
+      #       labels = Nokogiri::XML(File.open('uploads/recording/'+''+med_id.to_s+'/'+name)).xpath('//label')
+      #       file_string = ''
+      #       labels.each do |node|
+      #         text = node.text
+      #         if text != '!SENT_START'
+      #           file_string = file_string +' '+ text.downcase
+      #         end
+      #       end
+      #       # if the string contains the search string then it will add the record to the 'hits' array.
+      #       if file_string.include?(@search[0].downcase)
+      #         trans_search_hits.append(med_id)
+      #       end
+      #     end
+      #   end
+      # end
+      Dir.entries('public/uploads/recording/').each do |id|
+        if ids.include?( id.to_i )
+          if File.exist?('public/uploads/recording/'+''+id+'/'+'transcript.xml')
+            labels = Nokogiri::XML(File.open('public/uploads/recording/'+''+id+'/'+'transcript.xml')).xpath('//label')
             file_string = ''
             labels.each do |node|
               text = node.text
@@ -187,7 +205,7 @@ class PagesController < ApplicationController
             end
             # if the string contains the search string then it will add the record to the 'hits' array.
             if file_string.include?(@search[0].downcase)
-              trans_search_hits.append(med_id)
+              trans_search_hits.append(id.to_i)
             end
           end
         end
