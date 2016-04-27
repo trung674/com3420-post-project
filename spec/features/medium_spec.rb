3fd7e0c67d6ccbd34b75670229e97b39adc0f063
Danny <dheard2@sheffield.ac.uk>
2016-04-27 16:00:35 +0100

    Added uploading document test

--- spec/features/medium_spec.rb
+++ spec/features/medium_spec.rb
@@ -2,7 +2,18 @@ require 'rails_helper'
 
 describe 'Medium' do
 
-  specify 'I can upload an image' do
+  specify 'I can upload a document' do
+    document = FactoryGirl.build(:document, :with_approved_record)
+    record = document.records.first
+    visit new_document_path
+    fill_in 'Title', with: record.title
+    attach_file('File', File.absolute_path(document.upload.path))
+    fill_in 'Description', with: record.description
+    fill_in 'Email', with: document.contributor.email
+    check 'medium_copyright'
+    submit_form
+
+    expect(page).to have_content 'Upload successful'
   end
 
   specify 'I can view an upload with an approved record' do
